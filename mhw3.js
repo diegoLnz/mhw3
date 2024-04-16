964312f3014ed93bb2ba2ad83272d08850afc8fb
Diego Leanza <diegoleanza.ita@gmail.com>
2024-04-16 20:32:42 +0200

    feat: implemented posts and users api call (still no OAuth2)

--- mhw3.js
+++ mhw3.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", async function(){
+    const posts = await getPosts();
+    const users = await getUsers();
+
+    
     
 });
+
+async function getPosts(){
+    return await fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json());
+}
+
+async function getUsers(){
+    return await fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json());
+}
